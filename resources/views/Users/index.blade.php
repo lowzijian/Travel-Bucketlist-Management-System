e5cbe6648f854057e4f6d451eb43bb63d0ce18c2
jake <jakelow@datumcorp.com>
2020-02-07 14:08:05 +0800

    Add in filter field in Users.index

--- resources/views/Users/index.blade.php
+++ resources/views/Users/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -20,11 +19,35 @@
 <body style="min-height:80%">
     <div class="flex-center full-height col-md-12">
 
-        <div style="margin:15px;width:70%">
+        <div style="margin:15px;width:75%" class="col-md-12">
             <p class="title" style="margin-bottom:0px">Welcome back <span class="displayName">Chin Kai Xiang</span></p>
             <p>Total visited<i class="fa fa-city icon"></i> : <span class="num">25</span></p>
             <p class="caption">Last updated at 30-1-2020</p>
         </div>
+
+        <div style="margin:15px;width:80%" class="col-md-12 row">
+            <select class="input-form-container col-sm-3" id="filter_country" style="margin:5px">
+                <option value="default" selected disabled hidden>Select a Country</option>
+                <option value="Malaysia">Malaysia</option>
+                <option value="Thailand">Thailand</option>
+                <option value="Singapore">Singapore</option>
+                <option value="Vietnam">Vietnam</option>
+            </select>
+
+
+            <select class="input-form-container col-sm-3" id="filter_status" style="margin:5px">
+                <option value="default" selected disabled hidden>Select a status</option>
+                <option value="visited">Visited</option>
+                <option value="notvisited">Not visited</option>
+            </select>
+
+
+            <button type="button" class="btnPrimary col-sm-2" onclick="filterSelection()" style="margin:5px"> <i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
+            <button type="reset" class="btnCancel col-sm-2" onclick="clearSelection()" style="margin:5px">Clear</button>
+
+        </div>
+
+
 
         <div class="content" style="margin:15px;width:80%;overflow:scroll;">
             <section id="gallery">
@@ -93,7 +116,13 @@
                 </div><!-- End container -->
 
             </section>
+            <div style="margin:15px;" class="flex-center" id="filter_country_info">
+
+            </div>
+
+
         </div>
+
     </div>
 
 
@@ -111,6 +140,64 @@
                 }, 600);
             }
         );
+
+        // Filter selection
+        function filterSelection() {
+            const selected_country_field = document.getElementById('filter_country')
+            const selected_country = selected_country_field.options[selected_country_field.selectedIndex].value
+            const selected_status_field = document.getElementById('filter_status')
+            const selected_status = selected_status_field.options[selected_status_field.selectedIndex].value
+            const country_info = document.getElementById('filter_country_info')
+            console.log(selected_country, 'selected_country')
+            console.log(selected_status, 'selected_status')
+
+            if (selected_country != "default") {
+                country_info.innerHTML = `
+                <div class="row bucketListCard col-md-12 withMarginVertical">
+                <div class="row">
+                    <img class="col-md-3" src="https://restcountries.eu/data/afg.svg" alt="FlagOfTheCountry" style=" object-fit: cover; height: 100px;">
+                    <div class="col-md-9 column">
+                        <div class="row">
+                            <p class="locationTitle col-md-3">Afghanistan</p>
+                        </div>
+                        <div class="row">
+                            <p class="locationRegion col-md-3">Asia</p>
+                        </div>
+                        <div class="row">
+                            <p class="locationLabel col-md-3"> <i class="fa fa-landmark"></i> Captital City</p>
+                            <p class="col-md-3">Kabul</p>
+                        </div>
+                        <div class="row">
+                            <p class="locationLabel col-md-3"> <i class="fa fa-users"></i> Population</p>
+                            <p class="col-md-3">27657145</p>
+                        </div>
+                        <div class="row">
+                            <p class="locationLabel col-md-3"> <i class="fa fa-language"></i> Languages</p>
+                            <p class="col-md-3">Pashto,Uzbek,Turkmen</p>
+                        </div>
+                        <div class="row">
+                            <p class="locationLabel col-md-3"> <i class="fa fa-money-bill-wave"></i> Currencies</p>
+                            <p class="col-md-3">AFN <span class="caption">(؋)</span></p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+                `
+
+            } else {
+                country_info.innerHTML = ``
+            }
+
+        }
+
+        function clearSelection() {
+            $('select').prop('selectedIndex', 0);
+            const country_info = document.getElementById('filter_country_info')
+            country_info.innerHTML = ``
+
+        }
     </script>
 </body>
 @endsection
