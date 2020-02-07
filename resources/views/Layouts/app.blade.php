d443a684d277786b1e00ae66e445540b013972b1
jake <jakelow@datumcorp.com>
2020-02-07 12:45:26 +0800

    Add in swal 2 import script to app.blade.php

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -23,6 +23,13 @@
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <!--SWAL2-->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+    <script src="sweetalert2.all.min.js"></script>
+    <!-- Optional: include a polyfill for ES6 Promises for IE11 -->
+    <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
