9b3b04a3f674e55a3ba9e8bee3dc17f285eaa92c
jake <jakelow@datumcorp.com>
2020-02-07 16:04:17 +0800

    Add in header in users.blade.php

--- resources/views/Users/index.blade.php
+++ resources/views/Users/index.blade.php
@@ -1,158 +1,142 @@
-@extends('layouts.app')
+@extends('layouts.users')
 
 @section('content')
-<header>
-    <div class="container">
-        <img src="/asset/img/travel_bucketlist_thumbnail.png" alt="Travel Bucketlist thumbnail">
-        <h1 style="color:white;padding-left: 15px;">The Travel Bucketlist</h1>
+<div class="flex-center full-height col-md-12">
 
+    <div style="margin:15px;width:75%" class="col-md-12">
+        <p class="title" style="margin-bottom:0px">Welcome back <span class="displayName">Chin Kai Xiang</span></p>
+        <p>Total visited<i class="fa fa-city icon"></i> : <span class="num">25</span></p>
+        <p class="caption">Last updated at 30-1-2020</p>
     </div>
 
-    <nav class="container" style="padding-Bottom:0px">
-        <a class="navigationItem active" href="{{ url('/home')}}"><i class="fa fa-home"></i> Home</a>
-        <a class="navigationItem " href="{{ url('/create')}}"><i class="fa fa-plus-square"></i> Create</a>
-        <a class="navigationItem" href="{{ url('/countries')}}"><i class="fa fa-compass"></i> Countries</a>
-        <a class="navigationItem" href="{{ url('/')}}"><i class="fa fa-sign-out-alt"></i></a>
-    </nav>
-</header>
-
-<body style="min-height:80%">
-    <div class="flex-center full-height col-md-12">
-
-        <div style="margin:15px;width:75%" class="col-md-12">
-            <p class="title" style="margin-bottom:0px">Welcome back <span class="displayName">Chin Kai Xiang</span></p>
-            <p>Total visited<i class="fa fa-city icon"></i> : <span class="num">25</span></p>
-            <p class="caption">Last updated at 30-1-2020</p>
-        </div>
-
-        <div style="margin:15px;width:80%" class="col-md-12 row">
-            <select class="input-form-container col-sm-3" id="filter_country" style="margin:5px">
-                <option value="default" selected disabled hidden>Select a Country</option>
-                <option value="Malaysia">Malaysia</option>
-                <option value="Thailand">Thailand</option>
-                <option value="Singapore">Singapore</option>
-                <option value="Vietnam">Vietnam</option>
-            </select>
+    <div style="margin:15px;width:80%" class="col-md-12 row">
+        <select class="input-form-container col-sm-3" id="filter_country" style="margin:5px">
+            <option value="default" selected disabled hidden>Select a Country</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="Thailand">Thailand</option>
+            <option value="Singapore">Singapore</option>
+            <option value="Vietnam">Vietnam</option>
+        </select>
 
 
-            <select class="input-form-container col-sm-3" id="filter_status" style="margin:5px">
-                <option value="default" selected disabled hidden>Select a status</option>
-                <option value="visited">Visited</option>
-                <option value="notvisited">Not visited</option>
-            </select>
+        <select class="input-form-container col-sm-3" id="filter_status" style="margin:5px">
+            <option value="default" selected disabled hidden>Select a status</option>
+            <option value="visited">Visited</option>
+            <option value="notvisited">Not visited</option>
+        </select>
 
 
-            <button type="button" class="btnPrimary col-sm-2" onclick="filterSelection()" style="margin:5px"> <i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
-            <button type="reset" class="btnCancel col-sm-2" onclick="clearSelection()" style="margin:5px">Clear</button>
-
-        </div>
-
-
-
-        <div class="content" style="margin:15px;width:80%;overflow:scroll;">
-            <section id="gallery">
-                <div>
-                    <div id="image-gallery">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                                <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
-                                    <a href="https://unsplash.it/800"><img src="https://unsplash.it/800" class="img-responsive"></a>
-                                    <div class="img-overlay">
-                                        <i class="fa fa-plane" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div class="img-title-caption">
-                                    <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
-                                    <p class="travelTitle">Travel to Bangkok</p>
-                                    <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                                <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
-                                    <a href="https://unsplash.it/700"><img src="https://unsplash.it/700" class="img-responsive"></a>
-                                    <div class="img-overlay">
-                                        <i class="fa fa-plane" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div class="img-title-caption">
-                                    <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
-                                    <p class="travelTitle">Travel to Bangkok</p>
-                                    <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
-
-                                </div>
-                            </div>
-
-                            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                                <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
-                                    <a href="https://unsplash.it/600"><img src="https://unsplash.it/600" class="img-responsive"></a>
-                                    <div class="img-overlay">
-                                        <i class="fa fa-plane" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div class="img-title-caption">
-                                    <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
-                                    <p class="travelTitle">Travel to Bangkok</p>
-                                    <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
-
-                                </div>
-                            </div>
-
-                            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                                <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
-                                    <a href="https://unsplash.it/500"><img src="https://unsplash.it/500" class="img-responsive"></a>
-                                    <div class="img-overlay">
-                                        <i class="fa fa-plane" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                                <div class="img-title-caption">
-                                    <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
-                                    <p class="travelTitle">Travel to Bangkok</p>
-                                    <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
-                                </div>
-                            </div>
-                        </div><!-- End row -->
-                    </div><!-- End image gallery -->
-                </div><!-- End container -->
-
-            </section>
-            <div style="margin:15px;" class="flex-center" id="filter_country_info">
-
-            </div>
-
-
-        </div>
+        <button type="button" class="btnPrimary col-sm-2" onclick="filterSelection()" style="margin:5px"> <i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
+        <button type="reset" class="btnCancel col-sm-2" onclick="clearSelection()" style="margin:5px">Clear</button>
 
     </div>
 
 
-    <script>
-        // Gallery image hover
-        $(".img-wrapper").hover(
-            function() {
-                $(this).find(".img-overlay").animate({
-                    opacity: 1
-                }, 600);
-            },
-            function() {
-                $(this).find(".img-overlay").animate({
-                    opacity: 0
-                }, 600);
-            }
-        );
 
-        // Filter selection
-        function filterSelection() {
-            const selected_country_field = document.getElementById('filter_country')
-            const selected_country = selected_country_field.options[selected_country_field.selectedIndex].value
-            const selected_status_field = document.getElementById('filter_status')
-            const selected_status = selected_status_field.options[selected_status_field.selectedIndex].value
-            const country_info = document.getElementById('filter_country_info')
-            console.log(selected_country, 'selected_country')
-            console.log(selected_status, 'selected_status')
+    <div class="content" style="margin:15px;width:80%;overflow:scroll;">
+        <section id="gallery">
+            <div>
+                <div id="image-gallery">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                            <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
+                                <a href="https://unsplash.it/800"><img src="https://unsplash.it/800" class="img-responsive"></a>
+                                <div class="img-overlay">
+                                    <i class="fa fa-plane" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="img-title-caption">
+                                <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
+                                <p class="travelTitle">Travel to Bangkok</p>
+                                <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
+                            </div>
+                        </div>
 
-            if (selected_country != "default") {
-                country_info.innerHTML = `
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                            <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
+                                <a href="https://unsplash.it/700"><img src="https://unsplash.it/700" class="img-responsive"></a>
+                                <div class="img-overlay">
+                                    <i class="fa fa-plane" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="img-title-caption">
+                                <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
+                                <p class="travelTitle">Travel to Bangkok</p>
+                                <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                            <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
+                                <a href="https://unsplash.it/600"><img src="https://unsplash.it/600" class="img-responsive"></a>
+                                <div class="img-overlay">
+                                    <i class="fa fa-plane" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="img-title-caption">
+                                <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
+                                <p class="travelTitle">Travel to Bangkok</p>
+                                <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                            <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
+                                <a href="https://unsplash.it/500"><img src="https://unsplash.it/500" class="img-responsive"></a>
+                                <div class="img-overlay">
+                                    <i class="fa fa-plane" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="img-title-caption">
+                                <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
+                                <p class="travelTitle">Travel to Bangkok</p>
+                                <button class="btnOutlined" style="float:right;" onclick=" window.location.href= `{{ url('/show')}}`">Read More</button>
+                            </div>
+                        </div>
+                    </div><!-- End row -->
+                </div><!-- End image gallery -->
+            </div><!-- End container -->
+
+        </section>
+        <div style="margin:15px;" class="flex-center" id="filter_country_info">
+
+        </div>
+
+
+    </div>
+
+</div>
+
+
+<script>
+    // Gallery image hover
+    $(".img-wrapper").hover(
+        function() {
+            $(this).find(".img-overlay").animate({
+                opacity: 1
+            }, 600);
+        },
+        function() {
+            $(this).find(".img-overlay").animate({
+                opacity: 0
+            }, 600);
+        }
+    );
+
+    // Filter selection
+    function filterSelection() {
+        const selected_country_field = document.getElementById('filter_country')
+        const selected_country = selected_country_field.options[selected_country_field.selectedIndex].value
+        const selected_status_field = document.getElementById('filter_status')
+        const selected_status = selected_status_field.options[selected_status_field.selectedIndex].value
+        const country_info = document.getElementById('filter_country_info')
+        console.log(selected_country, 'selected_country')
+        console.log(selected_status, 'selected_status')
+
+        if (selected_country != "default") {
+            country_info.innerHTML = `
                 <div class="row bucketListCard col-md-12 withMarginVertical">
                 <div class="row">
                     <img class="col-md-3" src="https://restcountries.eu/data/afg.svg" alt="FlagOfTheCountry" style=" object-fit: cover; height: 100px;">
@@ -186,18 +170,17 @@
 
                 `
 
-            } else {
-                country_info.innerHTML = ``
-            }
-
-        }
-
-        function clearSelection() {
-            $('select').prop('selectedIndex', 0);
-            const country_info = document.getElementById('filter_country_info')
+        } else {
             country_info.innerHTML = ``
-
         }
-    </script>
-</body>
+
+    }
+
+    function clearSelection() {
+        $('select').prop('selectedIndex', 0);
+        const country_info = document.getElementById('filter_country_info')
+        country_info.innerHTML = ``
+
+    }
+</script>
 @endsection
--- resources/views/Users/show.blade.php
+++ resources/views/Users/show.blade.php
@@ -1,97 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.users')
 
 @section('content')
-<header>
-    <div class="container">
-        <img src="/asset/img/travel_bucketlist_thumbnail.png" alt="Travel Bucketlist thumbnail">
-        <h1 style="color:White;">The Travel Bucketlist</h1>
+
+<div class="row col-md-12">
+
+    <div class="col-md-2" style="margin:10px;">
+        <span class="author row"><i class="fa fa-at" style="padding-right:5px"></i>Chin Kai Xiang</span>
+
+        <span class="caption row">Last edited at 1 January 2020</span>
+        <div class="row divider"></div>
     </div>
 
-    <nav class="container" style="padding-Bottom:0px">
-        <a class="navigationItem " href="{{ url('/home')}}"><i class="fa fa-home"></i> Home</a>
-        <a class="navigationItem " href="{{ url('/create')}}"><i class="fa fa-plus-square"></i> Create</a>
-        <a class="navigationItem" href="{{ url('/countries')}}"><i class="fa fa-compass"></i> Countries</a>
-        <a class="navigationItem" href="{{ url('/')}}"><i class="fa fa-sign-out-alt"></i></a>
-    </nav>
-</header>
-
-<body>
-    <div class="row col-md-12">
-
-        <div class="col-md-2" style="margin:10px;">
-            <span class="author row"><i class="fa fa-at" style="padding-right:5px"></i>Chin Kai Xiang</span>
-
-            <span class="caption row">Last edited at 1 January 2020</span>
-            <div class="row divider"></div>
+    <div class="col-md-9" style="margin:10px;margin-top: 30px;">
+        <div class="row">
+            <p class="title col-md-12">Travel to Bangkok</p>
+            <p class="travelLocation col-md-12" style="font-size:18px;"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
+            <p class="hashtags col-md-12">#MeaningfulLife #Thailand</p>
         </div>
 
-        <div class="col-md-9" style="margin:10px;margin-top: 30px;">
-            <div class="row">
-                <p class="title col-md-12">Travel to Bangkok</p>
-                <p class="travelLocation col-md-12" style="font-size:18px;"><i class="fa fa-map-marker-alt"></i> Bangkok , Thailand</p>
-                <p class="hashtags col-md-12">#MeaningfulLife #Thailand</p>
+        <div class="row">
+            <p class="description col-md-12">I have really enjoyed being apart of this tour group. The tour gave me the chance to experience a good variety of things over the week. It was great to see Bangkok (temples, nightlife, markets and the boat ride). It was then lovely to relax in Khao Sok, Bottle Beach and during the river cruise in Koh Pha-ngan on the last day. If I had travelled alone I would not of had the same experience. It was great to have all the activities organised so I could just turn up and enjoy. I was very lucky also to have 4 other solo female travellers in my group as I had worried they would be all couples or groups of friends. Arthur has been a great tour guide over the week. He helped with any questions or problems I had and he was always around if we needed him. He was also always there to take photos and send them to us. Arthur is a lot of fun and was always up for a laugh, including everyone, and making sure we were having a good time. The transport was great also, a taxi or minibus was always waiting for us when we finished an activity or wanted to go somewhere. The accommodation was generally well located (with walking distance to busy areas). I felt it could have been a bit cleaner (ants in my bedroom). However, when I told Arthur about this he immediately organised fo rme to change rooms which made me much happier. Overall I really enjoyed the whole experience and would recommend this trip to a friend. I look forward to travelling on another tour with TruTravels in the future.</p>
+        </div>
+
+        <div class="row" style="align-items:center;justify-content:center;">
+            <img src="https://unsplash.it/800" class="img-responsive" style="object-fit:fill;height:350px;width:500px">
+        </div>
+
+        <div class="row divider"></div>
+
+        <div class="row">
+            <div class="col-md-12 row">
+                <p class="title col-sm-4">Travel Duration :</p>
+                <span style="display:grid; " class="col-sm-5">
+                    <span class="title" style="color:#BDB4D9">15 days</span>
+                    <span class="caption">Start from 11/7/2019 to 12/7/2019</span>
+                </span>
             </div>
 
-            <div class="row">
-                <p class="description col-md-12">I have really enjoyed being apart of this tour group. The tour gave me the chance to experience a good variety of things over the week. It was great to see Bangkok (temples, nightlife, markets and the boat ride). It was then lovely to relax in Khao Sok, Bottle Beach and during the river cruise in Koh Pha-ngan on the last day. If I had travelled alone I would not of had the same experience. It was great to have all the activities organised so I could just turn up and enjoy. I was very lucky also to have 4 other solo female travellers in my group as I had worried they would be all couples or groups of friends. Arthur has been a great tour guide over the week. He helped with any questions or problems I had and he was always around if we needed him. He was also always there to take photos and send them to us. Arthur is a lot of fun and was always up for a laugh, including everyone, and making sure we were having a good time. The transport was great also, a taxi or minibus was always waiting for us when we finished an activity or wanted to go somewhere. The accommodation was generally well located (with walking distance to busy areas). I felt it could have been a bit cleaner (ants in my bedroom). However, when I told Arthur about this he immediately organised fo rme to change rooms which made me much happier. Overall I really enjoyed the whole experience and would recommend this trip to a friend. I look forward to travelling on another tour with TruTravels in the future.</p>
-            </div>
-
-            <div class="row" style="align-items:center;justify-content:center;">
-                <img src="https://unsplash.it/800" class="img-responsive" style="object-fit:fill;height:350px;width:500px">
-            </div>
-
-            <div class="row divider"></div>
-
-            <div class="row">
-                <div class="col-md-12 row">
-                    <p class="title col-sm-4">Travel Duration :</p>
-                    <span style="display:grid; " class="col-sm-5">
-                        <span class="title" style="color:#BDB4D9">15 days</span>
-                        <span class="caption">Start from 11/7/2019 to 12/7/2019</span>
-                    </span>
-                </div>
-
-                <div class="col-md-12 row withMarginVertical">
-                    <p class="title col-sm-4">Overall Experience :</p>
-                    <span class="title col-sm-5" style="color:#BDB4D9">Happy</span>
-                </div>
-            </div>
-
-            <div class="withMarginVertical content" style="float:right;">
-                <button class="btnPrimary" type="submit" id="Edit" name="Edit">Edit <i class="fa fa-edit"></i> </button>
-                <button class="btnWarning" type="button" onclick="deleteContent()"> <i class="fa fa-trash-alt"></i> </button>
+            <div class="col-md-12 row withMarginVertical">
+                <p class="title col-sm-4">Overall Experience :</p>
+                <span class="title col-sm-5" style="color:#BDB4D9">Happy</span>
             </div>
         </div>
 
+        <div class="withMarginVertical content" style="float:right;">
+            <button class="btnPrimary" type="submit" id="Edit" name="Edit">Edit <i class="fa fa-edit"></i> </button>
+            <button class="btnWarning" type="button" onclick="deleteContent()"> <i class="fa fa-trash-alt"></i> </button>
+        </div>
     </div>
 
-    <script>
-        function deleteContent() {
-            Swal.fire({
-                title: 'Are you sure?',
-                text: 'Do you really want to delete this memorable travel record? This process cannot be undone.',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#e3342f',
-                cancelButtonColor: '#C1C1C1',
-                confirmButtonText: 'Delete',
-            }).then((result) => {
-                if (result.value) {
-                    Swal.fire({
-                        allowOutsideClick: false,
-                        title: 'Deleted!',
-                        text: 'This record has been deleted.',
-                        icon: 'success',
-                    }).then((result) => {
-                        if (result.value) {
-                            window.location.href = `{{ url('/home')}}`;
-                        }
-                    })
-                }
-            })
-        }
-    </script>
-</body>
+</div>
+
+<script>
+    function deleteContent() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Do you really want to delete this memorable travel record? This process cannot be undone.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#e3342f',
+            cancelButtonColor: '#C1C1C1',
+            confirmButtonText: 'Delete',
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire({
+                    allowOutsideClick: false,
+                    title: 'Deleted!',
+                    text: 'This record has been deleted.',
+                    icon: 'success',
+                }).then((result) => {
+                    if (result.value) {
+                        window.location.href = `{{ url('/home')}}`;
+                    }
+                })
+            }
+        })
+    }
+</script>
 
 
 
