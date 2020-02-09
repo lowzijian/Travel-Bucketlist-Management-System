0744ae9b78573680c512fcba34f6b960ed9020a3
jake <jakelow@datumcorp.com>
2020-02-09 15:59:32 +0800

    Restyle

--- resources/views/Users/index.blade.php
+++ resources/views/Users/index.blade.php
@@ -3,150 +3,188 @@
 @section('content')
 
 
-<!-- header welcome back message -->
-<div class="flex-center  col-md-12 withMarginVertical">
+<div class="col-md-12">
 
-    <div class="col-md-12 row header">
-        <div class="container">
-            <div class="col-md-8">
-                <div>
-                    <h1 class="title row"> Welcome back </h1>
-                    <h3 class="row caption2"> our fellow travel buddy </h3>
-                    <h3 class="displayName row" style="font-size: 3.5rem"> <span class="underline">{{$user->name}} </span></h3>
+    <!-- header welcome back message -->
+    <div class="flex-center  col-md-12 withMarginVertical">
+
+        <div class="col-md-12 row header">
+            <div class="container">
+                <div class="col-md-8">
+                    <div>
+                        <h1 class="title row"> Welcome back </h1>
+                        <h3 class="row caption2"> our fellow travel buddy </h3>
+                        <h3 class="displayName row" style="font-size: 3.5rem"> <span class="underline">{{$user->name}} </span></h3>
+                    </div>
                 </div>
-                <div class="withMarginVertical">
-                    <span class="totalItem row">Total travel bucketlist's items : <span class="num">{{count($items)}}</span></span>
-                    <span class="caption row">Last updated at {{$user->updated_at}}</span>
+
+                <div class="col-md-4">
+                    <img src="/asset/img/travel.svg" alt="Travel Bucketlist" class="img-header" />
                 </div>
+
             </div>
 
-            <div class="col-md-4">
-                <img src="/asset/img/travel.svg" alt="Travel Bucketlist" class="img-header" />
+        </div>
+    </div>
+
+    <!-- weather widget -->
+    <div class="col-md-12 withMarginVertical">
+
+        <a class="weatherwidget-io" href="https://forecast7.com/en/3d11101d73/cheras/" data-label_1="CHERAS" data-label_2="WEATHER" data-icons="Climacons Animated" data-mode="Current" data-theme="weather_one">CHERAS WEATHER</a>
+        <script>
+            ! function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = 'https://weatherwidget.io/js/widget.min.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, 'script', 'weatherwidget-io-js');
+        </script>
+
+    </div>
+
+
+    <div class="flex-center  col-md-12 withMarginVertical">
+
+        <h1 class="title row">Explore Your Stories</h1>
+        <div class="withMarginVertical">
+            <span class="totalItem row">Total travel bucketlist's items : <span class="num">{{count($items)}}</span></span>
+            <span class="caption row">Last updated at {{$user->updated_at}}</span>
+        </div>
+
+
+        @if (count($items) >> 0)
+
+        <div class="col-md-12 row" style="justify-content: center;">
+
+            <h2 class="col-sm-2" style="text-align: center;padding-top:12px;"> Filter By</h2>
+            <select class="filter-form-container col-sm-3" id="filter_country" style="margin:5px">
+                <option value="default" selected disabled hidden>Select a Country</option>
+                @foreach($countries as $country) {
+                <option value={{$country->id}}>{{$country->name}}</option>
+                }
+                @endforeach
+            </select>
+
+            <div class="displayFilteredCountry col-sm-3" style="display: none">
+                <span class="filteredCountryValue"></span>
+                <span class="filteredCountryClear"><i class="fas fa-times clearBtn"></i></span>
+            </div>
+
+
+            <select class="filter-form-container col-sm-3" id="filter_status" style="margin:5px">
+                <option value="default" selected disabled hidden>Select a status</option>
+                <option value="visited">Visited</option>
+                <option value="notvisited">Not visited</option>
+            </select>
+
+            <div class="displayFilteredStatus col-sm-3" style="display: none">
+                <span class="filteredStatusValue"></span>
+                <span class="filteredStatusClear"><i class="fas fa-times clearBtn"></i></span>
             </div>
 
         </div>
 
-    </div>
-</div>
-
-<!-- weather widget -->
-<div class="col-md-12 withMarginVertical">
-
-    <a class="weatherwidget-io" href="https://forecast7.com/en/3d11101d73/cheras/" data-label_1="CHERAS" data-label_2="WEATHER" data-icons="Climacons Animated" data-mode="Current" data-theme="weather_one">CHERAS WEATHER</a>
-    <script>
-        ! function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (!d.getElementById(id)) {
-                js = d.createElement(s);
-                js.id = id;
-                js.src = 'https://weatherwidget.io/js/widget.min.js';
-                fjs.parentNode.insertBefore(js, fjs);
-            }
-        }(document, 'script', 'weatherwidget-io-js');
-    </script>
-
-</div>
-
-<div class="flex-center  col-md-12 withMarginVertical">
-
-    @if (count($items) >> 0)
-
-    <div class="col-md-12" style="padding: 2.5rem 2.5rem 3rem 2.5rem;">
-        <select class="input-form-container col-sm-3" id="filter_country" style="margin:5px">
-            <option value="default" selected disabled hidden>Select a Country</option>
-            @foreach($countries as $country) {
-            <option value={{$country->id}}>{{$country->name}}</option>
-            }
-            @endforeach
-        </select>
 
 
-        <select class="input-form-container col-sm-3" id="filter_status" style="margin:5px">
-            <option value="default" selected disabled hidden>Select a status</option>
-            <option value="visited">Visited</option>
-            <option value="notvisited">Not visited</option>
-        </select>
-
-
-        <button type="button" class="btnPrimary col-sm-2" onclick="filterSelection()" style="margin:5px"> <i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
-        <button type="reset" class="btnCancel col-sm-2" onclick="clearSelection()" style="margin:5px">Clear</button>
-
-    </div>
-
-
-
-    <div class="content" style="margin:15px;width:100%;">
-        <section id="gallery">
-            <div>
-                <div id="image-gallery">
-                    <div class="row">
-                        @foreach($items as $item)
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                            <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
-                                <img src="/{{(json_decode($item->photos))[0]}}" class="img-responsive">
-                                <div class="img-overlay">
-                                    <i class="fa fa-plane" aria-hidden="true"></i>
+        <div class="content" style="margin:15px;width:100%;">
+            <section id="gallery">
+                <div>
+                    <div id="image-gallery">
+                        <div class="row">
+                            @foreach($items as $item)
+                            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                                <div class="img-wrapper" onclick=" window.location.href= `{{ url('/show')}}`">
+                                    <a href={{"/show/" . $item->id}}><img src="/{{(json_decode($item->photos))[0]}}" class="img-responsive"></a>
+                                    <div class="img-overlay">
+                                        <i class="fa fa-plane" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="img-title-caption">
+                                    <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> {{$item->city}}, {{$item->travel_bucket_country->name}}</p>
+                                    <p class="travelTitle">{{$item->title}}</p>
+                                    <a class="btnOutlined" style="float:right;" href={{"/show/" . $item->id}}>Read More</a>
                                 </div>
                             </div>
-                            <div class="img-title-caption">
-                                <p class="travelLocation"><i class="fa fa-map-marker-alt"></i> {{$item->city}}, {{$item->travel_bucket_country->name}}</p>
-                                <p class="travelTitle">{{$item->title}}</p>
-                                <a class="btnOutlined" style="float:right;" href={{"/show/" . $item->id}}>Read More</a>
-                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
-                    </div>
 
-                </div><!-- End image gallery -->
-            </div><!-- End container -->
+                    </div><!-- End image gallery -->
+                </div><!-- End container -->
 
-        </section>
-        <div style="margin:15px;" class="flex-center" id="filter_country_info"></div>
+            </section>
+            <div style="margin:15px;" class="flex-center" id="filter_country_info"></div>
 
 
-    </div>
-
-
-    @else
-    <div style="margin:15px;width:75%" class="col-md-12 row flex-center">
-        <img src="/asset/img/emptylist.svg" alt="Empty Travel Bucketlist" class="img-EmptyList" />
-        <div class="withMarginVertical" style="text-align: center;">
-            <h1>Empty Travel Bucket. </h1>
-            <p>Start to create a new travel bucket list item now!</p>
         </div>
+
+
+        @else
+        <div style="margin:15px;width:75%" class="col-md-12 row flex-center">
+            <img src="/asset/img/emptylist.svg" alt="Empty Travel Bucketlist" class="img-EmptyList" />
+            <div class="withMarginVertical" style="text-align: center;">
+                <h1>Empty Travel Bucket. </h1>
+                <p>Start to create a new travel bucket list item now!</p>
+            </div>
+        </div>
+
+        @endif
+
     </div>
 
-    @endif
+    <script>
+        // Gallery image hover
+        $(".img-wrapper").hover(
+            function() {
+                $(this).find(".img-overlay").animate({
+                    opacity: 1
+                }, 600);
+            },
+            function() {
+                $(this).find(".img-overlay").animate({
+                    opacity: 0
+                }, 600);
+            }
+        );
 
-</div>
+        $('#filter_country').on('change', function() {
+            $(this).add('.displayFilteredCountry').toggle();
+            var countries = <?php echo json_encode($countries); ?>;
+            $('.filteredCountryValue', '.displayFilteredCountry').html(countries[this.value].name);
+        });
 
-<script>
-    // Gallery image hover
-    $(".img-wrapper").hover(
-        function() {
-            $(this).find(".img-overlay").animate({
-                opacity: 1
-            }, 600);
-        },
-        function() {
-            $(this).find(".img-overlay").animate({
-                opacity: 0
-            }, 600);
-        }
-    );
+        $('.filteredCountryClear', '.displayFilteredCountry').click(function() {
+            $('#filter_country').val('default').fadeIn();
+            $('.displayFilteredCountry').toggle();
+            $('.filteredCountryValue', '.displayFilteredCountry').html('');
+        });
 
-    // Filter selection
-    function filterSelection() {
-        const selected_country_field = document.getElementById('filter_country')
-        const selected_country = selected_country_field.options[selected_country_field.selectedIndex].value
-        const selected_status_field = document.getElementById('filter_status')
-        const selected_status = selected_status_field.options[selected_status_field.selectedIndex].value
-        const country_info = document.getElementById('filter_country_info')
-        console.log(selected_country, 'selected_country')
-        console.log(selected_status, 'selected_status')
 
-        if (selected_country != "default") {
-            country_info.innerHTML = `
+        $('#filter_status').on('change', function() {
+            $(this).add('.displayFilteredStatus').toggle();
+            $('.filteredStatusValue', '.displayFilteredStatus').html(this.value);
+        });
+
+        $('.filteredStatusClear', '.displayFilteredStatus').click(function() {
+            $('#filter_status').val('default').fadeIn();
+            $('.displayFilteredStatus').toggle();
+            $('.filteredStatusValue', '.displayFilteredStatus').html('');
+        });
+
+        // Filter selection
+        function filterSelection() {
+            const selected_country_field = document.getElementById('filter_country')
+            const selected_country = selected_country_field.options[selected_country_field.selectedIndex].value
+            const selected_status_field = document.getElementById('filter_status')
+            const selected_status = selected_status_field.options[selected_status_field.selectedIndex].value
+            const country_info = document.getElementById('filter_country_info')
+            console.log(selected_country, 'selected_country')
+            console.log(selected_status, 'selected_status')
+
+            if (selected_country != "default") {
+                country_info.innerHTML = `
                 <div class="row bucketListCard col-md-12 withMarginVertical">
                 <div class="row">
                     <img class="col-md-3" src="https://restcountries.eu/data/afg.svg" alt="FlagOfTheCountry" style=" object-fit: cover; height: 100px;">
@@ -180,17 +218,20 @@
 
                 `
 
-        } else {
-            country_info.innerHTML = ``
+            } else {
+                country_info.innerHTML = ``
+            }
+
         }
 
-    }
+        function clearSelection() {
+            $('select').prop('selectedIndex', 0);
+            const country_info = document.getElementById('filter_country_info')
+            country_info.innerHTML = ``
 
-    function clearSelection() {
-        $('select').prop('selectedIndex', 0);
-        const country_info = document.getElementById('filter_country_info')
-        country_info.innerHTML = ``
+        }
+    </script>
 
-    }
-</script>
+
+</div>
 @endsection
--- resources/views/Users/show.blade.php
+++ resources/views/Users/show.blade.php
@@ -13,12 +13,12 @@
 
     <div class="col-md-9" style="margin:10px;margin-top: 30px;">
         <div class="row">
-            <h1 class="title col-md-12"><span class="underline">{{$travelBucketItem[0] -> title}}</span></h1>
+            <h1 class="title col-md-12">{{$travelBucketItem[0] -> title}}</h1>
             <p class="travelLocation col-md-12" style="font-size:18px;"><i class="fa fa-map-marker-alt"></i> {{$travelBucketItem[0] -> city}} , {{$travelBucketItem[0] -> countryName}}</p>
-            <p class="hashtags col-md-12">#MeaningfulLife #Thailand</p>
+            <p class="hashtags col-md-12">{{$travelBucketItem[0] -> caption}}</p>
         </div>
 
-        <div class="row">
+        <div class="row withMarginVertical">
             <p class="description col-md-12">{{$travelBucketItem[0] -> description}}</p>
         </div>
 
@@ -26,11 +26,12 @@
             <img src="/{{(json_decode($travelBucketItem[0]->photos))[0]}}" class="img-responsive" style="object-fit:fill;height:350px;width:500px">
         </div>
 
-        <div class="row divider"></div>
+
+        <div class="row divider withMarginVertical"></div>
 
         <div class="row">
             <div class="col-md-12 row">
-                <p class="title col-sm-4">Travel Duration :</p>
+                <h3 class="title col-sm-4">Travel Duration :</h3>
                 <span style="display:grid; " class="col-sm-5">
                     <span class="title" style="color:#BDB4D9">{{(strtotime($travelBucketItem[0] -> end_date)-strtotime($travelBucketItem[0] -> start_date))/86400}} days</span>
                     <span class="caption">Start from {{date("d/m/Y", strtotime($travelBucketItem[0] -> start_date))}} to {{date("d/m/Y", strtotime($travelBucketItem[0] -> end_date))}}</span>
@@ -38,7 +39,7 @@
             </div>
 
             <div class="col-md-12 row withMarginVertical">
-                <p class="title col-sm-4">Overall Experience :</p>
+                <h3 class="title col-sm-4">Overall Experience :</h3>
                 <span class="title col-sm-5" style="color:#BDB4D9">Happy</span>
             </div>
         </div>
